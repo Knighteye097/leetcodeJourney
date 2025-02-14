249d4e0e034c0fe575491f10c694bec736cf6640
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-02-15 03:05:43 +0530

    File updated at 2/15/2025, 3:05:43 AM

--- 36-ValidSudoku/36-ValidSudoku.java
+++ 36-ValidSudoku/36-ValidSudoku.java
@@ -1,3 +1,4 @@
+// Last updated: 2/15/2025, 3:05:43 AM
 class Solution {
     public boolean isValidSudoku(char[][] board) {
         int[][] rowChecker = new int[9][9];
@@ -9,27 +10,18 @@ class Solution {
         for(int i=0; i<ROWS; i++) {
             for(int j=0; j<COLS; j++) {
                 char ch = board[i][j];
-
                 if(ch != '.'){
-                    int num = ch - '0';
-                    int index = num - 1;
+                    int index = ch - '1';
+                    int boxIndex = 3*(i/3) + (j/3);
 
-                    if(rowChecker[i][index] == -1 || colChecker[j][index] == -1) {
+                    if(rowChecker[i][index] == -1 || 
+                        colChecker[j][index] == -1 || boxChecker[boxIndex][index] == -1){
                         return false;
-                    } else {
-                        rowChecker[i][index] = -1;
-                        colChecker[j][index] = -1;
                     }
 
-                    int boxRowIndex = 3*(i/3);
-                    boxRowIndex += (j/3);
-
-                    if(boxChecker[boxRowIndex][index] == -1) {
-                        // System.out.println("BOX checker failed for the index: row: " + boxRowIndex + " and index: " + index);
-                        return false;
-                    } else {
-                        boxChecker[boxRowIndex][index] = -1;
-                    }
+                    rowChecker[i][index] = -1;
+                    colChecker[j][index] = -1;
+                    boxChecker[boxIndex][index] = -1;
 
                 }
             }
