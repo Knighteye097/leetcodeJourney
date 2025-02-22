1cc431d0aeec04b5da896f6f1ea7962adaf998e7
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-02-22 21:53:46 +0530

    Create file

---
+++ 1963-MinimumNumberofSwapstoMaketheStringBalanced/1963-MinimumNumberofSwapstoMaketheStringBalanced.java
@@ -0,0 +1,42 @@
+class Solution {
+    public int minSwaps(String s) {
+        char[] arr = s.toCharArray();
+        int n = s.length();
+
+        int openingBrackets = 0;
+        int closingBrackets = 0;
+
+        int swaps = 0;
+
+        int itr = 0;
+        int secondItr = n-1;
+
+        while(itr<secondItr) {
+            if(arr[itr] == '[') {
+                openingBrackets++;
+            } else {
+                closingBrackets++;
+            }
+
+            if(closingBrackets>openingBrackets) {
+
+                while(arr[secondItr] != '[') {
+                    secondItr--;
+                }
+
+                char temp = arr[itr];
+                arr[itr] = arr[secondItr];
+                arr[secondItr] = temp;
+
+                swaps++;
+                openingBrackets++;
+                closingBrackets--;
+                secondItr--;
+            }
+
+            itr++;
+        }
+
+        return swaps++;
+    }
+}
