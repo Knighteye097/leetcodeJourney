87f90e18814eb5dc2fead7fc2b6991028f052f7a
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-02-27 22:07:54 +0530

    File updated at 2/27/2025, 10:07:54 PM

--- 18-4Sum/18-4Sum.java
+++ 18-4Sum/18-4Sum.java
@@ -1,3 +1,4 @@
+// Last updated: 2/27/2025, 10:07:54 PM
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
         Arrays.sort(nums);
@@ -7,14 +8,20 @@ class Solution {
         for(int i=0; i<n-3; i++) {
             if(i>0 && nums[i] == nums[i-1]) continue;
 
+            if ((long) nums[i] + nums[i + 1] + nums[i + 2] + nums[i + 3] > target) break;
+            if ((long) nums[i] + nums[n - 1] + nums[n - 2] + nums[n - 3] < target) continue;
+
             for(int j=i+1; j<n-2; j++) {
                 if(j>i+1 && nums[j] == nums[j-1]) continue;
+
+                if ((long) nums[i] + nums[j] + nums[j + 1] + nums[j + 2] > target) break;
+                if ((long) nums[i] + nums[j] + nums[n - 1] + nums[n - 2] < target) continue;
 
                 int first = j+1;
                 int last = n-1;
 
                 while(first < last) {
-                    long sum = (long)nums[i] + (long)nums[j] + (long)nums[first] + (long)nums[last];
+                    long sum = (long)nums[i] + nums[j] + nums[first] + nums[last];
 
                     if(sum == (long)target) {
                         res.add(List.of(nums[i], nums[j], nums[first], nums[last]));
