3caf75d7673e199f872055d935ca7ca6c5c3fdc4
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-05 22:40:53 +0530

    Create file

---
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -0,0 +1,20 @@
+class Solution {
+    public int numRescueBoats(int[] people, int limit) {
+        Arrays.sort(people);
+        int last = people.length - 1;
+        int start = 0;
+
+        int res = 0;
+
+        while(start <= last) {
+            if(people[last] + people[start] <= limit) {
+                start++;
+            }
+
+            res++;
+            last--;
+        }
+
+        return res;
+    }
+}
