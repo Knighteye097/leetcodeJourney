95fda0d2921cbdc9ef3db9bac887e924d82fe604
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-11 22:09:01 +0530

    File updated at 3/11/2025, 10:09:01 PM

--- 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
+++ 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
@@ -1,26 +1,20 @@
+// Last updated: 3/11/2025, 10:09:01 PM
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
-        int n = arr.length;
-        int window = 0;
-        int res = 0;
+        int n = arr.length, window = 0, res = 0;
         
         for(int i=0; i<k; i++) {
             window += arr[i];
         }
 
-        if(window/k >= threshold) {
-            res++;
-        }
+        if(window >= k * threshold) res++;
 
 
         
         for(int i=k; i<n; i++) {
-            window -= arr[i-k];
-            window += arr[i];
+            window = window + arr[i] - arr[i-k];
 
-            if(window/k >= threshold){
-                res++;
-            }    
+            if(window >= k * threshold) res++;   
         }
 
         return res;
