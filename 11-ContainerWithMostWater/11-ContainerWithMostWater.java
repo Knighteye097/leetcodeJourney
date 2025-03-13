3915ff00d5f442a9fa32f5cc9e72dbe35e71ef03
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-13 23:26:43 +0530

    File updated at 13/03/2025, 23:26:43

--- 11-ContainerWithMostWater/11-ContainerWithMostWater.java
+++ 11-ContainerWithMostWater/11-ContainerWithMostWater.java
@@ -1,24 +1,28 @@
-// Last updated: 3/2/2025, 10:21:04 PM
+// Last updated: 13/03/2025, 23:26:43
 class Solution {
-    static{
-        for(int i = 0; i < 500; i++){
-            maxArea(new int[]{0,0});
-        }
-       } 
-    public static int maxArea(int[] height) {
-        int left = 0;
-        int right = height.length - 1;
-        int maxArea = Integer.MIN_VALUE;
-        while (left < right) {
-            int h = Math.min(height[left], height[right]);
-            maxArea = Math.max(maxArea, h * (right - left));
-            while(left < right && height[left] <= h) {
-                left++;
+    public int maxArea(int[] height) {
+        int maxArea = 0;
+        int n = height.length;
+
+        int first = 0;
+        int last = n-1;
+
+        while(first < last) {
+            int currHeight = Math.min(height[first], height[last]);
+            int currLength = (last-first);
+            int currArea = currLength * currHeight;
+
+            maxArea = Math.max(maxArea, currArea);
+
+            while(first<last && height[first] <= currHeight) {
+                first++;
             }
-            while(left < right && height[right] <= h) {
-                right--;
+
+            while(first<last && height[last] <= currHeight) {
+                last--;
             }
         }
+
         return maxArea;
     }
 }
