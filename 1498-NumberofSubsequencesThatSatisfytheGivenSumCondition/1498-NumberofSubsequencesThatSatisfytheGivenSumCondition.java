0145578a3a68c1bd9646fa89e9105867f721ce17
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-14 22:31:41 +0530

    File updated at 14/03/2025, 22:31:40

--- 1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.java
+++ 1498-NumberofSubsequencesThatSatisfytheGivenSumCondition/1498-NumberofSubsequencesThatSatisfytheGivenSumCondition.java
@@ -1,50 +1,30 @@
-// Last updated: 3/3/2025, 11:45:30 PM
-
-//Approach:
-//Bruteforce:O(n^2)
-
-//Approach:2 using two pointers
-//step:1 sort the array(Sorting helps in efficiently finding the minimum and maximum values.)
-//step:2 use two pointers(left, right)as min and max of the subsequence
-//      2.1)If nums[left] + nums[right] <= target, count valid subsequences. it means the left and right index satify the given condition so we calculate the no of sebsequences that can form in this range and add to count and do mod. so it means the range satisfies so we move left++ so increase the pointer to find next  subsequence range that satifies the condition.(*****Use Precomputed Powers of 2 to count subsequences efficiently.)
-//      2.2) Otherwise, move right inward (right--). ( as sum of num[left]+nums[right]>target) we need to reduce the sum of min and max of subsequence.
-
-
-
+// Last updated: 14/03/2025, 22:31:40
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        int mod=1_000_000_007;
-        int n=nums.length;
-
-        // Sort the array for the two-pointer approach
         Arrays.sort(nums);
+        int n = nums.length;
+        int modulo = 1_000_000_007;
 
-        int power[]=new int[n];
-         power[0]=1;
-        // Precompute powers of 2 up to n
-         for(int i=1;i<n;i++){
-            power[i]=(power[i-1]*2)%mod;   //next no of subseq is = 2^n so 2^previous no of subseq
-         }
+        int[] power = new int[n];
+        power[0] = 1;
 
-         int left=0, right=n-1;
-         int count=0;
+        for(int i=1; i<n; i++) {
+            power[i] = (2*power[i-1])%modulo;
+        }
 
-         while(left<=right){
-            if(nums[left]+nums[right]<=target){
-                count=(count+power[right-left])%mod;   //here directly finding the diff of max and min to find no of subsequence.
-                left++; // Expand to include more subsequences
-            }else{
-                right--; // Reduce max element
+        int first = 0;
+        int last = n - 1;
+        int count = 0;
+
+        while(first <= last) {
+            if(nums[first] + nums[last]<=target){
+                count = (count + power[last-first])%modulo;
+                first++;
+            } else {
+                last--;
             }
-         }
-       
+        }
 
         return count;
-        
     }
 }
-
-//tc: nlogn for sorting
- //     n for looping
- // so tc: O(n logn)
- //sc:O(n)     
