3e5904491ac0b3161ce300da972e6d5afee8694a
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-21 23:42:36 +0530

    File updated at 21/03/2025, 23:42:35

--- 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
+++ 1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold/1343-NumberofSub-arraysofSizeKandAverageGreaterthanorEqualtoThreshold.java
@@ -1,4 +1,4 @@
-// Last updated: 3/20/2025, 1:37:09 AM
+// Last updated: 21/03/2025, 23:42:35
 class Solution {
     public int numOfSubarrays(int[] arr, int k, int threshold) {
         int n = arr.length, window = 0, res = 0;
