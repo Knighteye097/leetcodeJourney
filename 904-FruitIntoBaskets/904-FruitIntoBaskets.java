c7a3756742552ab048f925740340eecfc912c4d6
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-03-25 13:17:24 +0530

    File updated at 3/25/2025, 1:17:24 PM

--- 904-FruitIntoBaskets/904-FruitIntoBaskets.java
+++ 904-FruitIntoBaskets/904-FruitIntoBaskets.java
@@ -1,36 +1,28 @@
-// Last updated: 3/25/2025, 12:55:43 PM
+// Last updated: 3/25/2025, 1:17:24 PM
 class Solution {
     public int totalFruit(int[] fruits) {
-        int left = 0, right = 0, n = fruits.length;
-
+        int left = 0, n = fruits.length;
         int[] arr = {-1, -1};
-
         int maxRes = 0;
 
-        for(; right<n; right++) {
-            if(arr[0] == -1) {
+        for(int right = 0; right<n; right++) {
+            if(arr[0] == -1 || arr[0] == fruits[right]) {
                 arr[0] = fruits[right];
-            } else if(arr[0] != fruits[right] && arr[1] == -1){
+            } else if(arr[1] == -1 || arr[1] == fruits[right]){
                 arr[1] = fruits[right];
-            } else if(arr[1] != fruits[right] && arr[0] != fruits[right]){
+            } else {
                 maxRes = Math.max(maxRes, right-left);
-                left = right-1;
-                int prevFruit = fruits[left];
 
-                while(left>0 && fruits[left-1] == prevFruit){
+                left = right - 1;
+                while(left>0 && fruits[left-1] == fruits[left]){
                     left--;
                 }
 
-                arr[0] = prevFruit;
+                arr[0] = fruits[left];
                 arr[1] = fruits[right];
             }
         }
 
-        if(left != right) {
-            maxRes = Math.max(maxRes, right-left);
-        }
-
-
-        return maxRes;
+        return Math.max(maxRes, n-left);
     }
 }
