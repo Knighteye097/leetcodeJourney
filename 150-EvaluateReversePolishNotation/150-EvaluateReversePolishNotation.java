5808a063e415794c90058a9d881de1641970d4df
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-04-11 21:53:29 +0530

    File updated at 4/11/2025, 9:53:28 PM

--- 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
+++ 150-EvaluateReversePolishNotation/150-EvaluateReversePolishNotation.java
@@ -1,40 +1,33 @@
-// Last updated: 4/11/2025, 9:51:27 PM
+// Last updated: 4/11/2025, 9:53:28 PM
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Long> stack = new Stack<>();
+        Deque<Integer> stack = new ArrayDeque<>();
 
-        for(String str : tokens) {
-            if(isOperation(str) && !stack.empty()) {
-                Long num2 = stack.pop();
-                Long num1 = stack.pop();
-
-                Long res = calculate(num1, num2, str);
-
-                stack.push(res);
+        for (String token : tokens) {
+            if (isOperator(token)) {
+                int b = stack.pop();
+                int a = stack.pop();
+                stack.push(applyOperator(a, b, token));
             } else {
-                stack.push(Long.valueOf(str));
+                stack.push(Integer.parseInt(token));
             }
         }
 
-
-        return stack.pop().intValue();
+        return stack.pop();
     }
 
-    private boolean isOperation(String ch) {
-        return ch.equals("+") || ch.equals("-") || ch.equals("*") || ch.equals("/");
-    } 
+    private boolean isOperator(String token) {
+        return token.equals("+") || token.equals("-") ||
+               token.equals("*") || token.equals("/");
+    }
 
-    private Long calculate(Long num1, Long num2, String operation) {
-        switch(operation) {
-            case "+" : return num1 + num2;
-
-            case "-" : return num1 - num2;
-
-            case "/" : return num1/num2;
-
-            case "*" : return num1 * num2;
-
-            default : return 1L;
+    private int applyOperator(int a, int b, String op) {
+        switch (op) {
+            case "+": return a + b;
+            case "-": return a - b;
+            case "*": return a * b;
+            case "/": return a / b;
+            default: throw new IllegalArgumentException("Invalid operator: " + op);
         }
-    }  
+    }
 }
