cd932811d845b2749f21b9d9a67f82d3e722564c
Satyam Mishra <75328271+Knighteye097@users.noreply.github.com>
2025-05-08 00:00:40 +0530

    Time: 51 ms (89.54%), Space: 62.9 MB (5.12%) - LeetHub

--- 0883-car-fleet/0883-car-fleet.java
+++ 0883-car-fleet/0883-car-fleet.java
@@ -1,37 +1,35 @@
 class Solution {
     public int carFleet(int target, int[] position, int[] speed) {
-        Map<Integer, Integer> map = new HashMap<>();
         int n = position.length;
+        double[] timeTaken = new double[n];
+        Map<Integer, Integer> map = new HashMap<>();
 
-        for(int i=0; i<n; i++) {
-            map.put(position[i], speed[i]);
+        for(int i=0; i<position.length; i++) {
+            map.put(position[i], i);
         }
 
         Arrays.sort(position);
 
-        for (int i = 0; i < n / 2; i++) {
-        int temp = position[i];
-        position[i] = position[n - 1 - i];
-        position[n - 1 - i] = temp;
+        for(int i=0; i<n/2; i++) {
+            int temp = position[i];
+            position[i] = position[n-i-1];
+            position[n-i-1] = temp;
         }
 
-       double[] time = new double[n];
-        for (int i = 0; i < n; i++) {
-            int pos = position[i];
-            int spd = map.get(pos);
-            time[i] = (double)(target - pos) / spd;
+        for(int i=0; i<n; i++) {
+            timeTaken[i] = ((double)target-position[i])/speed[map.get(position[i])];
         }
 
-        int fleet = 0;
-        double lastTime = 0.0;
+        int res = 0;
+        Deque<Double> stack = new ArrayDeque<>();
 
-        for (int i = 0; i < n; i++) {
-            if (time[i] > lastTime) {
-                fleet++;
-                lastTime = time[i];
+        for(double time : timeTaken) {
+            if (stack.isEmpty() || time > stack.peek()) {
+                res++;
+                stack.push(time);
             }
         }
 
-        return fleet;
+        return res;
     }
 }
